930f2e7e4f9d0937d77a3846f7f48642ad4ed855
entelequeia <olaia96tb@gmail.com>
2024-09-11 19:18:40 +0000

    First commit

--- src/app.js
+++ src/app.js
@@ -1,11 +1,21 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
-
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let first = "Un ";
+
+  let who = ["apagón ", "perro ", "amigo ", "familiar "];
+  let action = ["cortó ", "se comió ", "me pidió prestado ", "rompió "];
+  let what = ["el wifi ", "mi trabajo escolar ", "el cuaderno ", "el papel "];
+  let when = [
+    "cuando iba a empezar ",
+    "mientras estaba cenando ",
+    "ayer ",
+    "cuando lo dejé encima de la mesa "
+  ];
+
+  let rdm2 = Math.floor(Math.random() * who.length);
+  let rdm3 = Math.floor(Math.random() * action.length);
+  let rdm4 = Math.floor(Math.random() * what.length);
+  let rdm5 = Math.floor(Math.random() * when.length);
+
+  document.querySelector("#excuse").innerHTML =
+    first + who[rdm2] + action[rdm3] + what[rdm4] + when[rdm5];
 };
